95107f70e603fcd79e79396bdfcc354594f1ed76
Natalia Fernandez <naicolsx@gmail.com>
2022-08-01 12:58:03 -0500

    adjustments media queries desktop and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,18 @@ themeButton.addEventListener('click', () => {
 })
 
 /*  SCROLL REVEAL ANIMATION  */
+const sr =  ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500, 
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {delay: 500})
+sr.reveal(`.home__social`, {delay: 600})
+sr.reveal(`.about__img, .contact__box`, {origin: 'left'})
+sr.reveal(`.about__data, .contact__form`, {origin: 'right'})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {interval: 100})
+
